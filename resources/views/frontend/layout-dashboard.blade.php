d8e5f81429ac58d7c47964698e7fe82f82a7bb2a
danibahri <danibahri45@gmail.com>
2024-10-01 10:42:15 +0700

    new landing page

--- resources/views/frontend/layout-dashboard.blade.php
+++ resources/views/frontend/layout-dashboard.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>E-Laundry - Admin</title>
 </head>
+
 <body>
     <x-sidebar></x-sidebar>
     @yield('content')
 </body>
+
 </html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>E-Laundry</title>
 </head>
+
 <body>
     <x-navbar></x-navbar>
     <main class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
@@ -14,4 +16,5 @@
     </main>
     <x-footer></x-footer>
 </body>
+
 </html>
